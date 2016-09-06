0181e29211dc01418821a097f1e0c2da6c488ed5
Caleb Sander <caleb.sander@gmail.com>
2016-09-06 10:27:03 -0400

    Added DcMotorController and DeviceInterfaceModule
    Also improved RegExs to match a wider set of situations

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-node download-source.js
-node extract-source.js
-node set-comments.js
+node download-source.js &&
+node extract-source.js &&
+node set-comments.js &&
 
 #~/Android/Sdk/sources/android-23
 javadoc -public -notimestamp -d docs -sourcepath ~/AppData/Local/Android/sdk/sources/android-23 $(find sources -name "*.java")
